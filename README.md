32164cfc71808331a7cac5528ecc9216d3783512
tumit <tumit@live.com>
2019-01-15 10:21:17 +0700

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# spring-rest
+
+## init project
+- Web
+- Lombok
---
+++ src/test/java/zyx/tumit/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,17 @@
+package zyx.tumit.springrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
