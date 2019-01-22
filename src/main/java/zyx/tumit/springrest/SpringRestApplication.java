318c6fded8fada9e7d9650854dc5f1447a81ce34
tumit <tumit@live.com>
2019-01-22 09:22:04 +0700

    test: add sandbox profile for test postgresql

--- src/main/java/zyx/tumit/springrest/SpringRestApplication.java
+++ src/main/java/zyx/tumit/springrest/SpringRestApplication.java
@@ -21,9 +21,9 @@ public class SpringRestApplication {
     public CommandLineRunner demo(FoodRepository repository) {
         return (args)  -> {
             log.info("args={}", args);
-//            repository.save(Food.builder().id(1L).name("Apple").calories(300F).build());
-//            repository.save(Food.builder().id(2L).name("Banana").calories(200F).build());
-//            repository.save(Food.builder().id(3L).name("Potato").calories(100F).build());
+//            repository.save(Food.builder().name("Apple").calories(300F).build());
+//            repository.save(Food.builder().name("Banana").calories(200F).build());
+//            repository.save(Food.builder().name("Potato").calories(100F).build());
         };
     }
 
